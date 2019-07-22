ea6b8a1a21c3b611fa359ffc6b0b93c8c0f28218
Kyle E. Mitchell <kyle@kemitchell.com>
2019-07-21 23:20:40 -0700

    Add TODOs for routes

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,10 @@ var send = require('send')
 var routes = module.exports = require('http-hash')()
 
 routes.set('/', require('./home-page'))
+// TODO: routes.set('/timeline', require('./timeline'))
+// TODO: routes.set('/mentions', require('./mentions'))
+// TODO: routes.set('/posts/:publicKey/:index', require('./post'))
+// TODO: routes.set('/users/:publicKey', require('./user'))
 
 staticFile('styles.css')
 
