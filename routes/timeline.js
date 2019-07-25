5c15015baedbd776262a09e9b3538cac667106b0
Kyle E. Mitchell <kyle@kemitchell.com>
2019-07-25 02:32:36 -0700

    Begin implementing /timeline route

---
+++ routes/timeline.js
@@ -0,0 +1,77 @@
+var authenticate = require('./authenticate')
+var escapeHTML = require('escape-html')
+var footer = require('./partials/footer')
+var header = require('./partials/header')
+var internalError = require('./internal-error')
+var runParallel = require('run-parallel')
+var seeOther = require('./see-other')
+
+module.exports = function (request, response) {
+  authenticate(request, function (error, session) {
+    if (error) return internalError(error)
+    if (!session) return seeOther(request, response, '/')
+    var email = session.email
+    if (session.email === 'manager') return seeOther(request, response, '/manage')
+    request.storage.account(email, function (error, account) {
+      if (error) return internalError(error)
+      var publicKey = account.publicKey
+      runParallel({
+        reduction: function (done) {
+          request.storage.reduction(publicKey, done)
+        },
+        timeline: function (done) {
+          var envelopes = []
+          request.storage.createTimelineStream(publicKey)
+            .on('data', function (envelope) {
+              envelopes.push(envelope)
+              if (envelopes.length === 100) {
+                this.destroy()
+                finish()
+              }
+            })
+            .once('finish', finish)
+          function finish () {
+            done(null, envelopes)
+          }
+        }
+      }, function (error, data) {
+        if (error) return internalError(error)
+        render(data)
+      })
+    })
+  })
+
+  function render (data) {
+    response.setHeader('Content-Type', 'text/html')
+    response.end(`
+<!doctype html>
+<html lang=en-US>
+  <head>
+    <meta charset=UTF-8>
+    <title>bentletter</title>
+  </head>
+  <body>
+    ${header()}
+    <main role=main>
+      ${renderTimeline(data)}
+    </main>
+    ${footer()}
+  </body>
+</html>
+    `.trim())
+  }
+}
+
+function renderTimeline (data) {
+  return `<ol>${data.timeline.map(function (envelope) {
+    return renderEnvelope(envelope, data.reduction)
+  }).join('')}</ol>`
+}
+
+function renderEnvelope (envelope, reduction) {
+  var publicKey = envelope.publicKey
+  var message = envelope.message
+  var index = message.index
+  var name = reduction.following[publicKey].name
+  return `<li>${escapeHTML(name)} #${index}</li>`
+}
